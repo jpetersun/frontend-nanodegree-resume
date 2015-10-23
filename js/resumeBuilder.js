d869822c1b6bfc2e41a76f85b6694ed34be1dc38
James T Peterson <jpeterson903@yahoo.com>
2015-10-23 14:43:47 -0700

    Adding gh pages

---
+++ js/resumeBuilder.js
@@ -0,0 +1,341 @@
+var work = {
+	"jobs": [
+		{
+			"employer": "Vince's Spaghetti Express",
+			"title": "Food preparer",
+			"location": "Temecula, CA, US",
+			"years": "2015-Present",
+			"description": "Preparing food to order and handling cash or credit card transactions."
+		},
+		{
+			"employer": "McDonald's",
+			"title": "Maintenance",
+			"location": "Temecula, CA, US",
+			"years": "2013-2015",
+			"description": "Performed general maintenance and cleaning duties."
+		},
+		{
+			"employer": "Millennium Financial Management",
+			"title": "Tax preparer Assistant",
+			"location": "Temecula, CA, US",
+			"years": "2010-2013",
+			"description": "Prepared personal and corporate taxes."
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "<a href='https://jpetersun.github.io/'</a>Portfolio Site",
+			"dates": "August 2015",
+			"url": "https://jpetersun.github.io/",
+			"description": "Built a custom personal website with images, descriptions, and links to other hosted projects. <br>Arranged HTML, CSS, and Bootstrap effectively to make website responsive and accessible across all platforms.",
+			"images": [
+					"images/portfolio-site-800_medium_2x.png"
+			]
+		},
+		{
+			"title": "<a href='https://jpetersun.github.io/frontend-nanodegree-resume/'</a>Interactive Resume",
+			"dates": "September 2015",
+			"description": "Developed an interactive resume read from a JSON file that dynamically displays content within a given template. <br>Constructed with JavaScript and jQuery making use of objects, functions, conditionals, and control structures.",
+			"images": [
+					"images/interactive-resume-800_medium_2x.jpg"
+			]
+		},
+		{
+			"title": "<a href='http://jpetersun.github.io/frontend-nanodegree-arcade-game/'</a>Classic Arcade Game Clone",
+			"dates": "September 2015",
+			"url": "http://jpetersun.github.io/frontend-nanodegree-arcade-game/",
+			"description": "Utilized art assets and game engine provided by Udacity to clone the platform arcade game Frogger. <br>Coded player, enemies, and movement using Object-Oriented JavaScript with the pseudo-classical pattern.",
+			"images": [
+					"images/arcade-game-800_medium_2x.jpg"
+			]
+		},
+		{
+			"title": "<a href='http://jpetersun.github.io/frontend-nanodegree-mobile-portfolio/'</a>Website Optimization",
+			"dates": "October 2015",
+			"url": "http://jpetersun.github.io/frontend-nanodegree-mobile-portfolio/",
+			"description": "Improved the critical rendering path to load a website under 1 second and to achieve a PageSpeed score above 90. <br>Implemented more efficient methods for a website to maintain 60 frames per second.",
+			"images": [
+					"images/web-perf-2-800_medium_2x.jpg"
+			]
+		}
+	]
+}
+
+var summary = {
+	"story": "My fascination for building and tinkering with computers naturally led me into programming as I've always enjoyed the process of learning and teaching myself. <br>This summer I experimented with an online calculus class and various coding tutorials to discover my passion for programming and the opportunities online education brings. <br> I decided to focus my attention on gaining the practical skills of becoming a front-end web developer."
+}
+
+var bio = {
+	"name": "James Peterson",
+	"role": "Front-End Developer",
+	"contacts": {
+		"mobile": "951-972-6767",
+		"email": "jpeterson903@yahoo.com",
+		"github": "jpetersun",
+		"location": "Temecula, CA, US"
+	},
+	"welcomeMessage": "Hello, I'm James, and I create captivating interactive experiences ",
+	"bioPic": "images/me.jpg",
+	"skills": [
+		"HTML5", "CSS3", "JavaScript", "jQuery", "Python"
+	]
+}
+
+var education = {
+	"schools": [
+		{
+			"name": "Udacity",
+			"location": "Mountainview, CA, US",
+			"degree": "Nanodegree",
+			"majors": "Front-End Web Development",
+			"dates": "2015 - 2015",
+			"url": "https://www.udacity.com/"
+		},
+		{
+			"name": "Palomar College",
+			"location": "San Marcos, CA, US",
+			"degree": "Dropout",
+			"majors": "General Education",
+			"dates": "August 2014 - May 2015",
+			"url": "http://www2.palomar.edu/"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Calculus One",
+			"school": "Coursera",
+			"dates": "May 2015 - July 2015",
+			"url": "https://www.coursera.org/learn/calculus1"
+		},
+		{
+			"title": "HTML & CSS",
+			"school": "Codecademy",
+			"dates": "August 2015",
+			"url": "https://www.codecademy.com/en/tracks/web"
+		},
+		{
+			"title": "JavaScript",
+			"school": "Codecademy",
+			"dates": "August 2015",
+			"url": "https://www.codecademy.com/en/tracks/javascript"
+		},
+
+		{
+			"title": "jQuery",
+			"school": "Codecademy",
+			"dates": "August 2015",
+			"url": "https://www.codecademy.com/en/tracks/jquery"
+		},
+		{
+			"title": "Intro to HTML and CSS",
+			"school": "Udacity",
+			"dates": "August 2015",
+			"url": "https://www.udacity.com/course/intro-to-html-and-css--ud304-nd"
+		},
+		{
+			"title": "Responsive Web Design Fundamentals",
+			"school": "Udacity",
+			"dates": "September 2015",
+			"url": "https://www.udacity.com/course/responsive-web-design-fundamentals--ud893"
+		},
+		{
+			"title": "Responsive Images",
+			"school": "Udacity",
+			"dates": "September 2015",
+			"url": "https://www.udacity.com/course/responsive-images--ud882-nd"
+		},
+		{
+			"title": "JavaScript Basics",
+			"school": "Udacity",
+			"dates": "September 2015",
+			"url": "https://www.udacity.com/course/javascript-basics--ud804-nd"
+		},
+		{
+			"title": "Intro to jQuery",
+			"school": "Udacity",
+			"dates": "September 2015",
+			"url": "https://www.udacity.com/course/intro-to-jquery--ud245-nd"
+		},
+		{
+			"title": "Object-Oriented JavaScript",
+			"school": "Udacity",
+			"dates": "September 2015",
+			"url": "https://www.udacity.com/course/object-oriented-javascript--ud015-nd"
+		},
+		{
+			"title": "HTML5 Canvas",
+			"school": "Udacity",
+			"dates": "September 2015",
+			"url": "https://www.udacity.com/course/html5-canvas--ud292-nd"
+		},
+		{
+			"title": "How to Use Git and GitHub",
+			"school": "Udacity",
+			"dates": "September 2015",
+			"url": "https://www.udacity.com/course/how-to-use-git-and-github--ud775-nd"
+		},
+		{
+			"title": "Website Performance Optimization",
+			"school": "Udacity",
+			"dates": "October 2015",
+			"url": "https://www.udacity.com/course/website-performance-optimization--ud884-nd"
+		},
+		{
+			"title": "Browser Rendering Optimization",
+			"school": "Udacity",
+			"dates": "October 2015",
+			"url": "https://www.udacity.com/course/browser-rendering-optimization--ud860-nd"
+		},
+		{
+			"title": "Intro to AJAX",
+			"school": "Udacity",
+			"dates": "October 2015",
+			"url": "https://www.udacity.com/course/intro-to-ajax--ud110-nd"
+		},
+		{
+			"title": "JavaScript Design Patterns",
+			"school": "Udacity",
+			"dates": "November 2015",
+			"url": "https://www.udacity.com/course/javascript-design-patterns--ud989-nd"
+		},
+		{
+			"title": "Learn Backbone.js",
+			"school": "Udacity",
+			"dates": "November 2015",
+			"url": "https://www.udacity.com/course/responsive-web-design-fundamentals--ud893"
+		},
+		{
+			"title": "JavaScript Testing",
+			"school": "Udacity",
+			"dates": "November 2015",
+			"url": "https://www.udacity.com/course/javascript-testing--ud549-nd"
+		},
+	]
+}
+
+work.display = function() {
+	for(job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].years);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+		$(".work-entry:last").append(formattedEmployerTitle, formattedDates, formattedDescription);
+	}
+}
+
+projects.display = function() {
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedUrl = HTMLprojectTitle.replace("#", projects.projects[project].url);
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedTitle);
+		$(".project-entry:last").append(formattedDates, formattedDescription);
+
+		if (projects.projects[project].images.length > 0) {
+			for (image in projects.projects[project].images) {
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
+}
+
+education.display = function() {
+	for (school in education.schools) {
+		$("#education").append(HTMLschoolStart);
+
+
+		var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+		$(".education-entry:last").append(formattedName + formattedDegree);
+		$(".education-entry:last").append(formattedDates, formattedLocation, formattedMajor);
+	}
+	$("#education").append(HTMLonlineClasses);
+	for (course in education.onlineCourses) {
+		$("#education").append(HTMLschoolStart);
+
+		var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+		var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+		var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+		var formattedURL = HTMLonlineURL.replace(/"#"/g, education.onlineCourses[course].url);
+		$(".education-entry:last").append(formattedTitle + formattedSchool);
+		$(".education-entry:last").append(formattedDates, formattedURL);
+	}
+}
+
+
+
+
+bio.display = function() {
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	var formattedImage = HTMLbioPic.replace("%data%", bio.bioPic);
+	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").prepend(formattedImage);
+	$("#header").append(formattedWelcomeMsg);
+	$("#topContacts").append(formattedMobile, formattedEmail, formattedGithub, formattedLocation);
+	$("#footerContacts").append(formattedMobile, formattedEmail, formattedGithub, formattedLocation);
+
+	var name = "James Peterson";
+	var role = "Web Developer";
+
+	var formattedName = HTMLheaderName.replace("%data%", name);
+	var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+
+		if(bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+
+		if (bio.skills.length > 0) {
+			for (skill in bio.skills) {
+				var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+				$("#skills:last").append(formattedSkill);
+			}
+		}
+	}
+}
+
+summary.display = function() {
+	var formattedSummary = HTMLsummary.replace("%data%", summary.story)
+	$("#summary").append(formattedSummary);
+}
+
+summary.display();
+education.display();
+projects.display();
+work.display();
+bio.display();
+
+function inName(name) {
+  name = name.trim().split(" ");
+  console.log(name);
+  name[1] = name[1].toUpperCase();
+  name[0] = name[0].slice(0,1).toUpperCase() +
+    name[0].slice(1).toLowerCase();
+  return name[0] + " " + name[1];
+}
+
+$("#mapDiv").append(googleMap);
+
+//$("#main").append(internationalizeButton);
+
+
+
+
